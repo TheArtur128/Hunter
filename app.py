56680c49b70cc62599d76f7674d3568c4aca9a5c
TheArtur128 <88573504+TheArtur128@users.noreply.github.com>
2021-08-22 21:28:15 +0700

    New very clumsy combat animation

--- app.py
+++ app.py
@@ -31,25 +31,34 @@ class Weapon(Primitive):
     coordinates = [(81, 8), (38, 48), (-8, 81), (-25, 35), (-5, 8), (-5, -27), (8, -5), (45, -15)]
     def __init__(self, name, img, damage=10, speed=FPS, x=0, y=0, vector=1, master=None):
         super().__init__(name, x, y)
+        #Атрибуты описывающие оружие непосредственно
         self.damage = damage
         self.speed = speed
+        #Ссылка на персонажа держущего оружие
         self.master = master
+        #Атрибуты действия
+        self.action = [0, 0, 0]
+        #Положение
         self.vector = vector
+        #Штраф при расположении под углом
         self.__coordinates = {}
         for i in range(1, 9):
             self.__coordinates[str(i)] = [img[str(i)], Weapon.coordinates[i-1]]
+        #Графическая чать оружия
         self.img = self.__class__.img
 
     def update_coordinates(self):
         if self.master is not None:
-            self.vector = self.master.vector
+            self.vector = self.master.vector + self.action[1]
+            self.vector = check_vector(self.vector)
             self.x = self.master.x + self.__coordinates[str(self.vector)][1][0]
             self.y = self.master.y + self.__coordinates[str(self.vector)][1][1]
 
 
 class Katana(Weapon):
     img = katana_image
-    def __init__(self, name, damage=10, speed=FPS, x=0, y=0, vector=1, master=None):
+    speed = FPS // 15
+    def __init__(self, name, damage=10, speed=speed, x=0, y=0, vector=1, master=None):
         super().__init__(name, katana_image, damage, speed, x, y, vector, master)
 
 
@@ -71,7 +80,6 @@ class Hunter(Primitive):
         }
         self.weapon = Katana("Sasai-kudasai", master=self)
 
-
     @property
     def speed(self): return self.__speed
     @property
@@ -91,18 +99,26 @@ while world_is_life:
         if action.type == pygame.QUIT:
             world_is_life = False
 
+        if action.type == pygame.MOUSEBUTTONDOWN:
+            if action.button == 1:
+                if Hero.action != "attack":
+                    Hero.action = "attack"
+
         if action.type == pygame.KEYDOWN:
-            #if action.key == pygame.K_SPACE: Hero.action = "attack"
-            if action.key == pygame.K_LEFT: Hero.movement["left"] = [True, random_pole()]
-            if action.key == pygame.K_RIGHT: Hero.movement["right"] = [True, random_pole()]
-            if action.key == pygame.K_UP: Hero.movement["up"] = [True, random_pole()]
-            if action.key == pygame.K_DOWN: Hero.movement["down"] = [True, random_pole()]
+            if action.key == pygame.K_z:
+                if Hero.action != "attack":
+                    Hero.action = "attack"
+
+            if action.key in key["LEFT"]: Hero.movement["left"] = [True, random_pole()]
+            if action.key in key["RIGHT"]: Hero.movement["right"] = [True, random_pole()]
+            if action.key in key["UP"]: Hero.movement["up"] = [True, random_pole()]
+            if action.key in key["DOWN"]: Hero.movement["down"] = [True, random_pole()]
 
         if action.type == pygame.KEYUP:
-            if action.key == pygame.K_LEFT: Hero.movement["left"] = [False, random_pole()]
-            if action.key == pygame.K_RIGHT: Hero.movement["right"] = [False, random_pole()]
-            if action.key == pygame.K_UP: Hero.movement["up"] = [False, random_pole()]
-            if action.key == pygame.K_DOWN: Hero.movement["down"] = [False, random_pole()]
+            if action.key in key["LEFT"]: Hero.movement["left"] = [False, random_pole()]
+            if action.key in key["RIGHT"]: Hero.movement["right"] = [False, random_pole()]
+            if action.key in key["UP"]: Hero.movement["up"] = [False, random_pole()]
+            if action.key in key["DOWN"]: Hero.movement["down"] = [False, random_pole()]
 
 
     #Если были нажаты две смежные кнопки то ставим нужный вектор и запрещаем его изминять
@@ -145,7 +161,6 @@ while world_is_life:
             elif Hero.vector in [8, 1, 2]:
                 Hero.vector += 1
 
-
     #Изминение y координаты
     if Hero.movement["up"][0]:
         if Hero.y > 0:
@@ -170,25 +185,31 @@ while world_is_life:
                 Hero.vector -= 1
 
 
-    #Перебрасываем вектор если перешли границу
-    if Hero.vector < 1:
-        Hero.vector = 8
-    elif Hero.vector > 8:
-        Hero.vector = 1
+    #Выполняем атаку
+    if Hero.action == "attack" and Hero.weapon is not None:
+        Hero.weapon.action[0] -= 1
+        if Hero.weapon.action[0] <= 0:
+            Hero.weapon.action[0] = Hero.weapon.__class__.speed
+            Hero.weapon.action[1] -= 1
+            Hero.weapon.action[2] += 1
+            if Hero.weapon.action[2] > 3:
+                Hero.weapon.action = [0, 0, 0]
+                Hero.action = "calmness"
 
-
-    #if Hero.action == "attack" and Hero.weapon is not None:
-    #    Hero.weapon.img = attack_image
-
+    #Переброска вектора и данных дублирующих его
+    Hero.vector = check_vector(Hero.vector)
+    Hero.weapon.vector = check_vector(Hero.weapon.vector)
 
     #Рисуем
     app.blit(fone_image["one"], (0, 0))
 
-    #Прорисовываем все обьекты в хранилище
+    #Работа с хранилищем
     for item in memory:
+        #Проверки для нужных обьектах
         if item.__class__ == Katana:
             item.update_coordinates()
 
+        #Прорисовываем все обьекты в хранилище
         app.blit(*item.drawing_data())
 
     pygame.display.update()
--- configuration.py
+++ configuration.py
@@ -5,6 +5,13 @@ import os
 import json
 from win32api import GetSystemMetrics as win
 
+#Группы кнопок которые в контесте приводят к одному результату
+key = {
+    "LEFT": [pygame.K_LEFT, pygame.K_a],
+    "RIGHT": [pygame.K_RIGHT, pygame.K_d],
+    "UP": [pygame.K_UP, pygame.K_w],
+    "DOWN": [pygame.K_DOWN, pygame.K_s]
+}
 
 #Парсим все картинки из папки в словарь под их имена
 def get_image(way):
@@ -33,6 +40,7 @@ katana_image = get_image("weapon/katana")
 #Загружаем папку person
 cricle_image = get_image("person/cricle")
 
+
 #Возврощает рандомный знак
 def random_pole():
     if random(0, 1) == 1:
@@ -41,15 +49,25 @@ def random_pole():
         return -1
 
 
+#Перебрасываем вектор если перешли границу
+def check_vector(arg):
+    if int(arg) < 1:
+        arg += 8
+    elif int(arg) > 8:
+        arg -= 8
+    return arg
+
+
 theme = "dark"
 
 #Получаем цвета из заданной темы
 with open(f"theme\{theme}-theme.json", "r") as file:
     color = json.loads(file.read())["color"]
 
-FPS = 30
-
 #Глобальные состояния
 time = True
+
+#Константы и системная информация
+FPS = 30
 
 window_dimensions = (600, 400)
